c6abbdf6b67acfe79562c6eb55230bcadd81be1c
fegs <ang0123dev@gmail.com>
2013-05-06 11:48:31 +0900

    잘못된 링크 수정

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ function getPosts(category_name, callback) {
 		for(var i in articles) {
 			if(articles[i].content_path != null) {
 				var file_path = path.join(default_path, articles[i].content_path, 'index.md');
-        var archive_path = 'public/archives/' + articles[i].content_path;
+        var archive_path = 'archives/' + category_name + '/' + articles[i].content_path;
 				try {
 					var file_content = fs.readFileSync(file_path, 'utf8');
           console.log(archive_path);
